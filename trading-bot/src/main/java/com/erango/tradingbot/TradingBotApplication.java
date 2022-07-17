ecf455a49a42acd608f3072a6f21d935aeefd598
eragol <eragol@gmail.com>
2022-07-17 19:15:59 +0300

    push from eclipse

--- trading-bot/src/main/java/com/erango/tradingbot/TradingBotApplication.java
+++ trading-bot/src/main/java/com/erango/tradingbot/TradingBotApplication.java
@@ -6,9 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TradingBotApplication {
 
+	//git push -u origin main --no-verify
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TradingBotApplication.class, args);
-		System.out.println("Erango");
+		System.out.println("Erango 19:15");
+		
 	}
 
 }
